28a67df152ea3706877106504e20c56d6af90411
Sysnettechs <njerishadrack@gmail.com>
2024-04-12 12:07:47 +0300

    Update LuciansLusciousLasagna.cs

--- exercises/concept/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ exercises/concept/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,20 @@
 class Lasagna
 {
-    // TODO: define the 'ExpectedMinutesInOven()' method
-
-    // TODO: define the 'RemainingMinutesInOven()' method
-
-    // TODO: define the 'PreparationTimeInMinutes()' method
-
-    // TODO: define the 'ElapsedTimeInMinutes()' method
+    	 public int ExpectedMinutesInOven()
+	 {
+		 return 40;
+	 }
+	 public int RemainingMinutesInOven(int actualTime)
+	 {
+		 return ExpectedMinutesInOven() - actualTime;
+	 }
+	 public int PreparationTimeInMinutes(int layers)
+	 {
+		 return layers * 2;
+	 }
+	 public int ElapsedTimeInMinutes(int layers, int actual)
+	 {
+		 return PreparationTimeInMinutes(layers) + actual;
+	 }
+}d
 }
